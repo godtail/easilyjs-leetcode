e13363cb21ca01508804686407568e287187467a
Xusheng Yang <easilyjs@gmail.com>
2023-11-15 14:37:01 +0800

    complete 122

---
+++ src/122. Best Time to Buy and Sell Stock II/max-profit.test.ts
@@ -0,0 +1,13 @@
+import { maxProfit } from './max-profit';
+
+describe('121. Best Time to Buy and Sell Stock', () => {
+  it('test case 1', () => {
+    const prices = [7, 1, 5, 3, 6, 4];
+    expect(maxProfit(prices)).toEqual(7);
+  });
+
+  it('test case 2', () => {
+    const prices = [1, 2, 3, 4, 5];
+    expect(maxProfit(prices)).toEqual(4);
+  });
+});
---
+++ src/122. Best Time to Buy and Sell Stock II/max-profit.ts
@@ -0,0 +1,12 @@
+export const maxProfit = (prices: number[]): number => {
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    // 使用贪心算法，只要有收益就进行买卖
+    if (prices[i] > prices[i - 1]) {
+      maxProfit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return maxProfit;
+};
